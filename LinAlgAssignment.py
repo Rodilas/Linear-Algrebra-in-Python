98fbe990adc452d9ca9d6a230a577a147e3e1ff6
Oscar Bengtsson <62248931+oscarbengtsson3@users.noreply.github.com>
2021-11-22 15:37:53 +0000

    Clean up

--- LinAlgAssignment.py
+++ LinAlgAssignment.py
@@ -25,7 +25,6 @@ class Vector:
     def __str__ (self):
         s = str(self.vector)
         return s
-        
 
     def __mul__(self, other):
         isinstance(other, (int,float))
@@ -37,7 +36,6 @@ class Vector:
     def __rmul__(self, other):
         return self.__mul__(other)
     
-    
     def innerProduct(self, other):
         assert isinstance(other, Vector)
         som = 0
@@ -48,87 +46,3 @@ class Vector:
 # Testing creating an object of Vector
 a = Vector([1, 2, 3])
 b = Vector([4, 5, 6])
-
-
-
-
-
-print(a+b)
-
-
-"""
-
-#soma de vetores
-a = [3, 5, -5, 8]
-b = [4, 7, 9, -4]
-
-print("Vector a = ", a)
-print("Vector b = ", b)
-
-#soma de vetores
-sum = []
-for i in range(len(a)):
-    sum.append(a[i] + b[i])
-
-print("Vector Addition = ", sum)
-
-
-#inner product
-
-"""
-
-a = [1, 2, 3]
-b = [4, 5, 6]
-#32
-soma= 0
-for i in range(len(a)):
-    
-    soma += a[i]*b[i]
-    
-print(soma)
-
-"""
-#cross product
-
-"""
-
-a = [1, 2, 3]
-b = [4, 5, 6]
-
-Mult = []
-
-for i in range(len(a)):
-    Mult.append(0)
-    for j in range(len(a)):
-        if j != i:
-            for k in range(len(a)):
-                if k != i:
-                    if k > j:
-                        Mult[i] += a[j]*b[k]
-                    elif k < j:
-                        Mult[i] -= a[j]*b[k]
-print(Mult)
-        
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
