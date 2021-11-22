d4e27931b37657e1a8604777eb25e1efa25d620a
Rodilas <rodrigo.club13@gmail.com>
2021-11-22 14:43:45 +0000

    added alogrithms

--- LinAlgAssignment.py
+++ LinAlgAssignment.py
@@ -18,3 +18,99 @@ b = Vector([4, 5, 6])
 print("pls work")
 
 
+
+"""
+
+#soma de vetores
+a = [3, 5, -5, 8]
+b = [4, 7, 9, -4]
+
+print("Vector a = ", a)
+print("Vector b = ", b)
+
+#soma de vetores
+sum = []
+for i in range(len(a)):
+    sum.append(a[i] + b[i])
+
+print("Vector Addition = ", sum)
+
+
+"""
+
+#multiplicar vector por integer/float
+
+
+"""
+
+a = [1, 2, 3]
+n = 2.1
+
+Mult = []
+for j in range(len(a)):
+    Mult.append(a[j]*n)
+    
+print(Mult)    
+
+"""
+
+
+#inner product
+
+"""
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+#32
+soma= 0
+for i in range(len(a)):
+    
+    soma += a[i]*b[i]
+    
+print(soma)
+
+"""
+#cross product
+
+"""
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+Mult = []
+
+for i in range(len(a)):
+    Mult.append(0)
+    for j in range(len(a)):
+        if j != i:
+            for k in range(len(a)):
+                if k != i:
+                    if k > j:
+                        Mult[i] += a[j]*b[k]
+                    elif k < j:
+                        Mult[i] -= a[j]*b[k]
+print(Mult)
+        
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
