2706b21631a0e1955b57b73bd7ece9de7daeebda
emilhene <em3524en-s@student.lu.se>
2021-12-10 18:05:15 +0000

    App try

--- Application.py
+++ Application.py
@@ -1,9 +1,35 @@
 from Classes import Vector, Matrix, MatrixReader, LinearSystemSolver
 
-# Temporary setup. We need to figure out want kind of application the professor wants.
+#A farmer wants to know how many acres of Wheat, Corn and Cauliflower he should grow.
+
+a = Matrix([[1, 1, 1], 
+             [10, 4, 2], 
+             [0, 1, 1]])
+
+b = Vector([5.8, 40, 2.75])
 
 
-file = "Matrix_file_" + str(input("Choose matrix dimension (10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120): ")) + ".txt"
-reader = MatrixReader(file)
-m = Matrix(reader.load())
-print(m)
+
+
+
+
+
+a2 = Matrix([[1,1,1,0,0,0,0,0,0,0],
+             [0,0,0,1,1,1,0,0,0,0],
+             [0,0,0,0,0,0,1,1,1,1],
+             [1,0,0,1,0,0,1,0,0,0],
+             [0,1,0,0,1,0,0,1,0,0],
+             [0,0,1,0,0,1,0,0,1,0],
+             [500,700,600,0,0,0,0,0,0,0],
+             [0,0,0,500,700,600,0,0,0,0],
+             [0,0,0,0,0,0,500,700,600,0],
+             [(1/15),(1/15),(1/15),0,0,0,(-1/18),(-1/18),(-1/18),0],
+             ])
+
+b2 = Vector([15,25,18,8,10,30,8500,15300,10200,0])
+
+c = LinearSystemSolver(a2, b2)
+
+weights = c.solve()
+
+print(weights)
