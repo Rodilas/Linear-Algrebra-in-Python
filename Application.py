cd4e448defb4020ac1b9e36e61d093ebcfdf1ed9
J-Silvestre <90836876+J-Silvestre@users.noreply.github.com>
2021-12-12 11:28:21 +0000

    application update

--- Application.py
+++ Application.py
@@ -1,86 +1,89 @@
 from Classes import Vector, Matrix, LinearSystemSolver
 
-
-# Question 1:
-print("\n")
-print("--------------Question 1------------------")
-print("\n")
-
+#Question 1 input
 m1 = Matrix([[1, 1, 1], 
              [10, 4, 2], 
-             [0, 1, 1]])
+             [0, 10, 10]])
 
-v1 = Vector([5.8, 40, 2.75])
+v1 = Vector([5.8, 40, 27.5])
 
-c = LinearSystemSolver(m1, v1)
-solution1 = c.solve()
-product_names1 = ["wheat", "potatoes", "carrots"]
-product_profit1 = [50, 100, 100]
-profit1 = 0
+c1 = LinearSystemSolver(m1, v1)
+solution1 = c1.solve()
 
-for i in range (len(solution1)):
-    profit1 += product_profit1[i]*round((solution1[i][0]),5)
-    print("The farmer produces", round((solution1[i][0]),5),"tons of " + product_names1[i])
+#Question 2 input
+m2 = Matrix([[1, 1, 1, 1], 
+             [10, 4, 2, 5], 
+             [0, 10, 10, 0],
+             [0.5,-0.5,-0.5,-0.5]])
+
+v2 = Vector([5.8, 40, 27.5, 0])
+
+c2 = LinearSystemSolver(m2, v2)
+solution2 = c2.solve()
+
+
+def main():
+    value1 = True
+    value2 = True
+    
+    for i in range(len(solution1)): #if all values in solution vector 1 are positive
+        if solution1[i][0] < 0:
+            value1 = False
+
+    for i in range(len(solution2)): #if all values in solution vector 2 are positive
+        if solution2[i][0] < 0:
+            value2 = False
+
+    if value1 == True and value2 == True: #if both solutions return positive values
+        return application(solution1, solution2)
+    else:
+        s5 = print("The solution is not feasible")
+        return s5
+
+
+
+def application(solution_1, solution_2):
+    # Question 1:
+    print("\n")
+    print("--------------Question 1------------------")
+    print("\n")
+
+    product_names1 = ["wheat", "potatoes", "carrots"]
+    product_profit1 = [50, 100, 100]
+    tons1 = [5, 10, 10]
+    profit1 = 0
+
+    for i in range (len(solution1)):
+        profit1 += product_profit1[i]*round(((solution1[i][0])*tons1[i]),3)
+        print("The farmer produces", round(((solution1[i][0])*tons1[i]),3),"tons of " + product_names1[i])
+    print("The farmer profits: ", round((profit1),3))
 
 
 # Question 2:
-print("The farmer profits", profit1)
-print("\n")
-print("--------------Question 2------------------")
-print("\n")       
+    print("\n")
+    print("--------------Question 2------------------")
+    print("\n")       
 
-m2 = Matrix([[1, 1, 1, 1], 
-             [10, 4, 2, 5], 
-             [0, 1, 1, 0],
-             [0.5,-0.5,-0.5,-0.5]])
+    product_names2 = ["wheat", "potatoes", "carrots", "Cauliflower"]
+    product_profit2 = [50, 100, 100, 200]
+    tons2 = [5, 10, 10, 20]
+    profit2 = 0
 
-v2 = Vector([5.8, 40, 2.75, 0])
+    for i in range (len(solution2)):
+        profit2 += product_profit2[i]*round(((solution2[i][0])*tons2[i]),3)
+        print("The farmer produces", round(((solution2[i][0])*tons2[i]),3),"tons of " + product_names2[i])
+    print("The farmer profits", round((profit2),3))
 
+    if profit2 > profit1:
+        print("The farmer's profit increased by", round((profit2-profit1),3))
+    else:
+        print("The farmer's profit decreased by", round((profit1-profit2),3))
 
-c = LinearSystemSolver(m2, v2)
-solution2 = c.solve()
-product_names2 = ["wheat", "potatoes", "carrots", "Cauliflower"]
-product_profit2 = [50, 100, 100, 200]
-profit2 = 0
+    if solution2[0][0] > solution1[0][0]:
+        print("The government's new measure was successful! It increased wheat production by"
+              , round((solution2[0][0] - solution1[0][0]),3))
+    else:
+        print("The government's new measure was not successful! It decreased wheat production by", 
+              round((solution1[0][0]-solution2[0][0]),3), "tons")
 
-for i in range (len(solution2)):
-    profit2 += product_profit2[i]*round((solution2[i][0]),5)
-    print("The farmer produces", round((solution2[i][0]),5),"tons of " + product_names2[i])
-
-print("The farmer profits", profit2)
-
-if profit2 > profit1:
-    print("The farmer's profit increased by", profit2-profit1)
-else:
-    print("The farmer's profit decreased by", profit1-profit2)
-
-if solution2[0][0] > solution1[0][0]:
-    print("The government's new measure was successful! It increased wheat production by"
-          , solution2[0][0] - solution1[0][0])
-else:
-    print("The government's new measure was not successful! It decreased wheat production by", 
-          round((solution1[0][0]-solution2[0][0]),5), "tons")
-
-
-
-
-
-"""a2 = Matrix([[1,1,1,0,0,0,0,0,0,0],
-             [0,0,0,1,1,1,0,0,0,0],
-             [0,0,0,0,0,0,1,1,1,1],
-             [1,0,0,1,0,0,1,0,0,0],
-             [0,1,0,0,1,0,0,1,0,0],
-             [0,0,1,0,0,1,0,0,1,0],
-             [500,700,600,0,0,0,0,0,0,0],
-             [0,0,0,500,700,600,0,0,0,0],
-             [0,0,0,0,0,0,500,700,600,600],
-             [(1/15),(1/15),(1/15),0,0,0,(-1/18),(-1/18),(-1/18),(-1/18)],
-             ])
-
-b2 = Vector([15,25,18,8,10,30,8500,15300,10200,0])
-
-c = LinearSystemSolver(a2, b2)
-
-weights = c.solve()
-
-print(weights)"""
+main()
